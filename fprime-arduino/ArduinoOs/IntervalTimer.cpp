07225e08893f754f80263db1e2342e3ad3584670
M Starch <LeStarch@googlemail.com>
2020-07-31 18:14:09 -0700

    lestarch: initial commit of the arduino os

---
+++ fprime-arduino/ArduinoOs/IntervalTimer.cpp
@@ -0,0 +1,41 @@
+#include <Os/IntervalTimer.hpp>
+#include <Fw/Types/Assert.hpp>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <Arduino.h>
+
+namespace Os {
+    
+    IntervalTimer::IntervalTimer() {
+        memset(&this->m_startTime,0,sizeof(this->m_startTime));
+        memset(&this->m_stopTime,0,sizeof(this->m_stopTime));
+    }
+
+    IntervalTimer::~IntervalTimer() {
+    }
+
+    void IntervalTimer::getRawTime(RawTime& time) {
+        U32 msec = millis();
+        U32 usec = micros();
+
+        time.upper = msec/1000;
+        time.lower = usec;
+    }
+
+    // Adapted from: http://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
+    // should be t1In - t2In
+    U32 IntervalTimer::getDiffUsec(const RawTime& t1In, const RawTime& t2In) {
+
+        RawTime result = {t1In.upper - t2In.upper,0};
+
+        if (t1In.lower < t2In.lower) {
+            result.upper -= 1; // subtract nsec carry to seconds
+            result.lower = t1In.lower + (1000000000 - t2In.lower);
+        } else {
+            result.lower = t1In.lower - t2In.lower;
+        }
+
+        return result.upper*1000000 + result.lower / 1000;
+    }
+}
---
+++ fprime-arduino/ArduinoOs/StreamLog.hpp
@@ -0,0 +1,21 @@
+/*
+ * StreamLog.hpp:
+ *
+ * A file defining the functions used to set and configure the Stream used in the Arduino Log::Os
+ * handler function.
+ *
+ *  Created on: Apr 13, 2019
+ *      Author: lestarch
+ */
+#include <Arduino.h>
+#ifndef OS_ARDUINO_STREAMLOG_HPP_
+#define OS_ARDUINO_STREAMLOG_HPP_
+
+namespace Os {
+/**
+ * Sets the global A_STREAM arduino stream used to log Arudino messages through a hardware "Steam"
+ * like UART or I2C.
+ */
+void setArduinoStreamLogHandler(Stream* stream);
+}
+#endif /* OS_ARDUINO_STREAMLOG_HPP_ */
