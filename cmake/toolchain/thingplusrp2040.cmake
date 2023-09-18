e6a184aae4a304ec60db25c9537e5779a59b4884
Ethan Chee <61472206+ethancheez@users.noreply.github.com>
2023-09-18 12:13:33 -0700

    Fix basic timer bug (#14)

--- cmake/toolchain/thingplusrp2040.cmake
+++ cmake/toolchain/thingplusrp2040.cmake
@@ -9,7 +9,7 @@ set(CMAKE_SYSTEM_PROCESSOR "arm")
 set(CMAKE_CROSSCOMPILING 1)
 set(FPRIME_USE_BAREMETAL_SCHEDULER ON)
 
-
 set(ARDUINO_FQBN "rp2040:rp2040:sparkfun_thingplusrp2040")
+add_compile_options(-DUSE_BASIC_TIMER)
 # Run the base arduino setup which should detect settings!
 include("${CMAKE_CURRENT_LIST_DIR}/support/arduino-support.cmake")
