43b3a0a09b6881eb53e2227e4bea9509a6fcf519
Kevin F. Ortega <kevin.f.ortega@gmail.com>
2025-05-21 10:09:32 -0700

    adding platform files for arm

--- cmake/platform/arm/Platform/PlatformTypes.h
+++ cmake/platform/arm/Platform/PlatformTypes.h
@@ -1,61 +1,53 @@
+
 /**
- * \brief PlatformTypes.h: for teensy systems (supports 64bit)
+ * \brief PlatformTypes.h C-compatible type definitions for Linux/Darwin
+ *
+ * PlatformTypes.h is typically published by platform developers to define
+ * the standard available arithmetic types for use in fprime. This standard
+ * types header is designed to support standard Linux/Darwin (unix) distributions
+ * running on x86, x86_64, arm, and arm64 machines and using the standard gcc/clang
+ * compilers shipped with the operating system.
  */
-#ifndef TEENSY_PLATFORM_TYPES_H_
-#define TEENSY_PLATFORM_TYPES_H_
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
 
-#include <cstdint>
-#include <cinttypes>
-#include <limits>
-
-#define FW_HAS_64_BIT    1 //!< Architecture supports 64 bit integers
-#define FW_HAS_32_BIT    1 //!< Architecture supports 32 bit integers
-#define FW_HAS_16_BIT    1 //!< Architecture supports 16 bit integers
-#define FW_HAS_F64    1 //!< Architecture supports 64 bit floating point numbers
-
-typedef int32_t PlatformIntType;
-#define PRI_PlatformIntType "ld"
-
-typedef uint32_t PlatformUIntType;
-#define PRI_PlatformUIntType "lu"
-
-typedef PlatformIntType PlatformIndexType;
-#define PRI_PlatformIndexType PRI_PlatformIntType
-
-typedef PlatformUIntType PlatformSizeType;
-#define PRI_PlatformSizeType PRI_PlatformUIntType
-
-typedef PlatformIntType PlatformSignedSizeType;
-#define PRI_PlatformSignedSizeType PRI_PlatformSignedSizeType
-
-typedef PlatformIntType PlatformAssertArgType;
-#define PRI_PlatformAssertArgType PRI_PlatformIntType
-
-typedef PlatformIntType PlatformTaskPriorityType;
-#define PRI_PlatformTaskPriorityType PRI_PlatformIntType
-
-typedef PlatformIntType PlatformQueuePriorityType;
-#define PRI_PlatformQueuePriorityType PRI_PlatformIntType
-
-#ifndef PLATFORM_POINTER_CAST_TYPE_DEFINED
-  // Check for __SIZEOF_POINTER__ or cause error
-  #ifndef __SIZEOF_POINTER__
-    #error "Compiler does not support __SIZEOF_POINTER__, cannot use Linux/Darwin types"
-  #endif
-
-  // Pointer sizes are determined by size of compiler
-  #if __SIZEOF_POINTER__ == 8
-    typedef uint64_t PlatformPointerCastType;
-    #define PRI_PlatformPointerCastType PRIx64
-  #elif __SIZEOF_POINTER__ == 4
-    typedef uint32_t PlatformPointerCastType;
-    #define PRI_PlatformPointerCastType PRIx32
-  #elif __SIZEOF_POINTER__ == 2
-    typedef uint16_t PlatformPointerCastType;
-    #define PRI_PlatformPointerCastType PRIx16
-  #else
-    typedef uint8_t PlatformPointerCastType;
-    #define PRI_PlatformPointerCastType PRIx8
-  #endif
+#ifdef  __cplusplus
+extern "C" {
 #endif
-#endif //TEENSY_PLATFORM_TYPES_H_
+#include <stdint.h>
+
+typedef int PlatformIntType;
+#define PRI_PlatformIntType "d"
+
+typedef unsigned int PlatformUIntType;
+#define PRI_PlatformUIntType "u"
+
+// Linux/Darwin definitions for pointer have various sizes across platforms
+// and since these definitions need to be consistent we must ask the size.
+// Check for __SIZEOF_POINTER__ or cause error
+#ifndef __SIZEOF_POINTER__
+#error "Compiler does not support __SIZEOF_POINTER__, cannot use Linux/Darwin types"
+#endif
+
+// Pointer sizes are determined by compiler
+#if __SIZEOF_POINTER__ == 8
+typedef uint64_t PlatformPointerCastType;
+#define PRI_PlatformPointerCastType PRIx64
+#elif __SIZEOF_POINTER__ == 4
+typedef uint32_t PlatformPointerCastType;
+#define PRI_PlatformPointerCastType PRIx32
+#elif __SIZEOF_POINTER__ == 2
+typedef uint16_t PlatformPointerCastType;
+#define PRI_PlatformPointerCastType PRIx16
+#elif __SIZEOF_POINTER__ == 1
+typedef uint8_t PlatformPointerCastType;
+#define PRI_PlatformPointerCastType PRIx8
+#else
+#error "Expected __SIZEOF_POINTER__ to be one of 8, 4, 2, or 1"
+#endif
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  // PLATFORM_TYPES_H_
