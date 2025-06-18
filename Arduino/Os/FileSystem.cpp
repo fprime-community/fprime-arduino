9e298d7128f5281b908e0d3a3e17c266edf335b8
Ethan Chee <61472206+ethancheez@users.noreply.github.com>
2025-06-18 07:00:56 -1000

    Add Os_File_Arduino_SD (#43)

---
+++ Arduino/Os/FileSystem.cpp
@@ -0,0 +1,60 @@
+// ======================================================================
+// \title Os/Arduino/File.cpp
+// \brief Arduino implementation for Os::File
+// ======================================================================
+#include "Arduino/Os/FileSystem.hpp"
+
+namespace Os {
+namespace Arduino {
+
+ArduinoFileSystem::Status ArduinoFileSystem::_removeDirectory(const char* path) {
+    if (!SD.exists(path)) {
+        return Status::INVALID_PATH;
+    }
+    if (SD.rmdir(path)) {
+        return Status::OP_OK;
+    }
+    return Status::OTHER_ERROR;
+}
+
+ArduinoFileSystem::Status ArduinoFileSystem::_removeFile(const char* path) {
+    if (!SD.exists(path)) {
+        return Status::INVALID_PATH;
+    }
+    if (SD.remove(path)) {
+        return Status::OP_OK;
+    }
+    return Status::OTHER_ERROR;
+}
+
+ArduinoFileSystem::Status ArduinoFileSystem::_rename(const char* originPath, const char* destPath) {
+    Status stat;
+    
+    stat = Os::FileSystem::copyFile(originPath, destPath);
+    if (stat != OP_OK) {
+        return stat;
+    }
+
+    stat = this->_removeFile(originPath);
+
+    return stat;
+}
+
+ArduinoFileSystem::Status ArduinoFileSystem::_getWorkingDirectory(char* path, FwSizeType bufferSize) {
+    return Status::NOT_SUPPORTED;
+}
+
+ArduinoFileSystem::Status ArduinoFileSystem::_changeWorkingDirectory(const char* path) {
+    return Status::NOT_SUPPORTED;
+}
+
+ArduinoFileSystem::Status ArduinoFileSystem::_getFreeSpace(const char* path, FwSizeType& totalBytes, FwSizeType& freeBytes) {
+    return Status::NOT_SUPPORTED;
+}
+
+FileSystemHandle* ArduinoFileSystem::getHandle() {
+    return &this->m_handle;
+}
+
+} // namespace Arduino
+} // namespace Os
